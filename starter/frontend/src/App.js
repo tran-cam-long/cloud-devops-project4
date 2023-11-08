9d7a5b4b62cdc81955c2adb8685dc2f7a964b97f
tran-cam-long <camlong.tran0702@gmail.com>
2023-11-08 15:30:07 +0700

    test action

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -3,6 +3,7 @@ import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import './App.css';
 
+// haha
 export default function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
